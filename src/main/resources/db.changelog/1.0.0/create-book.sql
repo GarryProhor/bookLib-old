f9329835b45b099cbbc85c6ce4e01f730e9686d7
Гарри Прохор <84273694+GarryProhor@users.noreply.github.com>
2022-01-30 21:58:59 +0300

    fix liquibase

--- src/main/resources/db.changelog/1.0.0/create-book.sql
+++ src/main/resources/db.changelog/1.0.0/create-book.sql
@@ -1,5 +1,5 @@
-CREATE TABLE emprecord.book(
-    id int,
+CREATE TABLE book(
+    id int auto_increment,
     isbn varchar(255) not null,
     name varchar(255) not null,
     author varchar(255) not null,
@@ -7,5 +7,4 @@ CREATE TABLE emprecord.book(
     weight double,
     price decimal,
     PRIMARY KEY(id)
-)
-    Next_command
+);
