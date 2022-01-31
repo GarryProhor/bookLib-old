6c8273ac0e51f3823f9c7c598f33cf52bd174a2e
Garry <igorprohorchenko@gmail.com>
2022-01-31 14:51:22 +0300

    liquibase fixes

--- src/main/resources/db.changelog/1.0.0/insert-book.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO book (isbn, name, author, page, weight, price)
-VALUES('ISBN 978-1-55111-123-9', 'Star Wars', 'G.Lucas', 800, 0.9, 1.99);
-
-INSERT INTO book (isbn, name, author, page, weight, price)
-VALUES('ISBN 978-1-55111-123-9', 'Garry Potter', 'G.Rouling', 700, 0.78, 99.98);
-
-select * from BOOK
