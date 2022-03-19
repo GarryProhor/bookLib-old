11c0111c4738fb85e3b7b022a5a92674a42652d3
Гарри Прохор <84273694+GarryProhor@users.noreply.github.com>
2022-03-19 23:04:56 +0300

    migration,model

---
+++ src/main/java/by/prohor/booklib/service/repo/UserRepo.java
@@ -0,0 +1,10 @@
+package by.prohor.booklib.service.repo;
+
+import by.prohor.booklib.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+
+}
